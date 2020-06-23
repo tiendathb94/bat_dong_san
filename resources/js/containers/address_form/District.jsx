92a28f21f52c80c785bfd0134b35a1b508366dcf
Quyen bui <buiquyen1995@gmail.com>
2020-06-23 23:25:19 +0700

    Add form create project

--- resources/js/containers/address_form/District.jsx
+++ resources/js/containers/address_form/District.jsx
@@ -2,13 +2,16 @@ import React, { Component } from 'react'
 import axios from "axios"
 import config from "../../config"
 import PropTypes from 'prop-types'
+import Styles from './address_form.module.scss'
+import classnames from 'classnames'
 
 class District extends Component {
     constructor (props) {
         super(props)
 
         this.state = {
-            districts: []
+            districts: [],
+            loading: false,
         }
     }
 
@@ -24,11 +27,13 @@ class District extends Component {
 
     async fetchDistricts () {
         if (!this.state.provinceId) {
+            this.setState({ districts: [] })
             return
         }
 
+        this.setState({ loading: true })
         const response = await axios.get(`${config.api.baseUrl}/address/districts-by-province?province_id=${this.state.provinceId}`)
-        this.setState({ districts: response.data })
+        this.setState({ districts: response.data, loading: false })
     }
 
     onChange = (event) => {
@@ -41,9 +46,20 @@ class District extends Component {
 
     render () {
         return (
-            <div>
+            <div className="form-group">
+                {
+                    this.state.loading && <div className={classnames('spinner-border', Styles.Loading)} role="status">
+                        <span className="sr-only">Loading...</span>
+                    </div>
+                }
+
                 <label>Quận/Huyện</label>
-                <select onChange={this.onChange} value={this.state.value}>
+                <select
+                    onChange={this.onChange}
+                    value={this.state.value}
+                    className="form-control"
+                    disabled={this.state.districts.length < 1 ? 'disabled' : ''}
+                >
                     <option>-- Quận/Huyện --</option>
                     {
                         this.state.districts && this.state.districts.map((district) => (
--- resources/js/pages/project/Components/CreateForm.jsx
+++ resources/js/pages/project/Components/CreateForm.jsx
@@ -2,10 +2,62 @@ import React, { Component } from 'react'
 import AddressForm from "../../../containers/address_form"
 
 class CreateForm extends Component {
+    constructor (props) {
+        super(props)
+
+        this.state = {
+            formValues: {}
+        }
+    }
+
+    onSyncAddress = (address) => {
+        this.setState({ formValues: { ...this.state.formValues, address: address } })
+    }
+
+    setFormFieldValue = (event) => {
+        this.setState({ formValues: { ...this.state.formValues, [event.target.name]: event.target.value } })
+    }
+
     render () {
         return (
             <div>
-                <AddressForm/>
+                <div className="container">
+                    <div className="row">
+                        <div className="form-group col">
+                            <label>Tên dự án</label>
+                            <input
+                                value={this.state.formValues.long_name}
+                                onChange={this.setFormFieldValue}
+                                placeholder="Nhập tên dự án"
+                                className="form-control"
+                            />
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="form-group col">
+                            <label>Tên ngắn của dự án</label>
+                            <input
+                                value={this.state.formValues.short_name}
+                                onChange={this.setFormFieldValue}
+                                placeholder="Nhập tên ngắn của dự án"
+                                className="form-control"
+                            />
+                        </div>
+
+                        <div className="form-group col">
+                            <label>Quy mô dự án</label>
+                            <input
+                                value={this.state.formValues.project_scale}
+                                onChange={this.setFormFieldValue}
+                                placeholder="Mô tả quy mô dự án"
+                                className="form-control"
+                            />
+                        </div>
+                    </div>
+                </div>
+
+                <AddressForm onSync={this.onSyncAddress}/>
             </div>
         )
     }
