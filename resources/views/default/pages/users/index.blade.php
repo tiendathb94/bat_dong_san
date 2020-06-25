76e953c35ffdd9df83b2906801fbc9ecf20db4a4
Thang Bui <thangbui.071192@gmail.com>
2020-06-25 09:38:34 +0700

    fix review

--- resources/views/default/pages/users/index.blade.php
+++ resources/views/default/pages/users/index.blade.php
@@ -61,70 +61,9 @@
             </div>
 
             <div class="col-9 p-0">
-                <div class="moduletitle">
-                    QUẢN LÝ TIN RAO BÁN, CHO THUÊ
-                </div>
-                <table class="w-100 my-3">
-                    <tbody>
-                        <tr>
-                            <td class="colorblue w-25">Từ ngày</td>
-                            <td class="colorblue w-25">Đến ngày</td>
-                            <td class="colorblue w-25">Loại tin</td>
-                            <td class="colorblue w-25">Trạng thái</td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <div class="pr-3">
-                                    <input name="" type="date" value="" class="form-control fs-12">
-                                </div>
-                            </td>
-                            <td>
-                                <div class="pr-3">
-                                    <input name="" type="date" value="" class="form-control fs-12">
-                                </div>
-                            </td>
-                            <td>
-                                <div class="pr-3">
-                                    <select name="" class="form-control fs-12">
-                                        <option value="7">Chọn loại tin</option>
-                                        <option value="0">Tin VIP đặc biệt</option>
-                                        <option selected="selected" value="1">Tin VIP 1</option>
-                                        <option value="2">Tin VIP 2</option>
-                                        <option value="3">Tin VIP 3</option>
-                                        <option value="4">Tin Ưu Đãi</option>
-                                        <option value="5">Tin Thường</option>
-                                    </select>
-                                </div>
-                            </td>
-                            <td>
-                                <div class="pr-3">
-                                    <select name="" class="form-control fs-12">
-                                        <option value="0">Tất cả</option>
-                                        <option value="1">Còn hạn</option>
-                                        <option selected="selected" value="2">Hết hạn</option>
-
-                                    </select>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td colspan="4" class="colorblue">Mã tin/ tiêu đề</td>
-                        </tr>
-                        <tr>
-                            <td class="pr-3">
-                                <input name="" type="text" class="form-control fs-12">
-                            </td>
-                            <td colspan="3">
-                                <input type="button" name="" value="Tìm kiếm" class="btn-search" autopostback="true">
-                                <span class="colorblue">(Lưu ý khi nhập mã tin/ tiêu đề thì các bộ lọc khác không có tác dụng)</span>
-                                <div class="btn-download-excel">
-                                    <span class="ti-download"></span>Excel
-                                </div>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-
+            @php($block = 'default.pages.users.blocks.')
+                @include($block . 'title', ['title' => 'QUẢN LÝ TIN RAO BÁN, CHO THUÊ'])
+                @include($block . 'search')
                 <table class="table table-bordered fs-12 text-center">
                     <thead>
                         <tr class="bg-thead">
@@ -144,8 +83,8 @@
                 <span class="font-weight-bold mt-5 mb-3 fs-12">Note</span>
                 <div class="fs-12">
                 Trong trường hợp Quý khách muốn đăng và quản lý tin rao tiếng Anh, xin vui lòng click vào đây 
-                <img src="https://file4.batdongsan.com.vn/images/no-photo.jpg" width="24px">&nbsp;
-                    <a title="" href="/user-page" class="text-dark"><strong>English</strong></a>
+                <img src="/images/no-photo.jpg" width="24px">&nbsp;
+                    <a title="" href="#" class="text-dark"><strong>English</strong></a>
                 </div>
             </div>
         </div>
@@ -155,8 +94,6 @@
 @push('styles')
     <link rel="stylesheet"
           href="{{ asset('css/pages/users/index.css') . '?m=' . filemtime('css/pages/users/index.css') }}">
-    <link rel="stylesheet"
-    href="{{ asset('themify-icons/themify-icons.css') }}">
 @endpush
 
 @push('scripts')
