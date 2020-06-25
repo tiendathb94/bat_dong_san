4a4723b6b5bb56a821b28332d19a8572f3069c3b
Quyen bui <buiquyen1995@gmail.com>
2020-06-25 18:26:31 +0700

    Add personal menu

--- routes/web/project.php
+++ routes/web/project.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::group(['guard' => 'web', 'middleware' => 'auth'], function () {
+Route::group(['guard' => 'web', 'middleware' => ['auth', 'permission']], function () {
     Route::get('/project/create', 'Web\ProjectController@create')->defaults('_config', [
         'view' => 'default.pages.project.create'
     ])->name('pages.project.create');
