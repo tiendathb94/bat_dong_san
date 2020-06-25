b4ad7584d12d3842632abbc9d4bd19455fc181da
Thang Bui <thangbui.071192@gmail.com>
2020-06-25 09:38:42 +0700

    fix review

---
+++ resources/views/default/pages/users/blocks/search.blade.php
@@ -0,0 +1,59 @@
+<table class="w-100 my-3">
+    <tbody>
+        <tr>
+            <td class="colorblue w-25">Từ ngày</td>
+            <td class="colorblue w-25">Đến ngày</td>
+            <td class="colorblue w-25">Loại tin</td>
+            <td class="colorblue w-25">Trạng thái</td>
+        </tr>
+        <tr>
+            <td>
+                <div class="pr-3">
+                    <input name="" type="date" value="" class="form-control fs-12">
+                </div>
+            </td>
+            <td>
+                <div class="pr-3">
+                    <input name="" type="date" value="" class="form-control fs-12">
+                </div>
+            </td>
+            <td>
+                <div class="pr-3">
+                    <select name="" class="form-control fs-12">
+                        <option value="7">Chọn loại tin</option>
+                        <option value="0">Tin VIP đặc biệt</option>
+                        <option selected="selected" value="1">Tin VIP 1</option>
+                        <option value="2">Tin VIP 2</option>
+                        <option value="3">Tin VIP 3</option>
+                        <option value="4">Tin Ưu Đãi</option>
+                        <option value="5">Tin Thường</option>
+                    </select>
+                </div>
+            </td>
+            <td>
+                <div class="pr-3">
+                    <select name="" class="form-control fs-12">
+                        <option value="0">Tất cả</option>
+                        <option value="1">Còn hạn</option>
+                        <option selected="selected" value="2">Hết hạn</option>
+                    </select>
+                </div>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="4" class="colorblue">Mã tin/ tiêu đề</td>
+        </tr>
+        <tr>
+            <td class="pr-3">
+                <input name="" type="text" class="form-control fs-12">
+            </td>
+            <td colspan="3">
+                <input type="button" name="" value="Tìm kiếm" class="btn-search" autopostback="true">
+                <span class="colorblue">(Lưu ý khi nhập mã tin/ tiêu đề thì các bộ lọc khác không có tác dụng)</span>
+                <div class="btn-download-excel">
+                    <span class="ti-download"></span>Excel
+                </div>
+            </td>
+        </tr>
+    </tbody>
+</table>
