b1f4582925dbb315594850b59ae31e6b7b18efdd
Thang Bui <thangbui.071192@gmail.com>
2020-06-25 09:45:34 +0700

    fix review

---
+++ resources/views/default/pages/users/blocks/sidebar.blade.php
@@ -0,0 +1,49 @@
+<div class="box-header">
+    <h3>TRANG CÁ NHÂN</h3>
+</div>
+<div class="box-arround">
+    <div class="useravatar">
+        <img class="avatar" src="/images/default-user-avatar-blue.jpg">
+        <span class="userfullname">{{ $user->fullname }}</span>
+        <!-- <div class="userpoint-menu">
+            <div class="userpoint-menu-level">
+                <label>Mã chuyển khoản: BDS14108898</label>
+            </div>
+        </div> -->
+        <div class="userpoint-menu">
+            <div class="userpoint-menu-level">
+                <label style="background-image:url(/images/rate-1.0.png);">Tài khoản: Thường</label>
+            </div>
+            <!-- <div class="userpoint-menu-point">
+                <label>0</label>
+                <span>&nbsp;điểm&nbsp;</span>
+                <a class="ti-info-alt" href="#">
+                    <div>
+                        <ul>
+                            <li>
+                                <span>Điểm đổi thưởng <b>0</b> điểm</span>
+                            </li>
+                            <li>
+                                <span>Đã tích lũy 0 VNĐ. Cần thêm 12.000.000 VNĐ để lên hạng Đồng</span>
+                            </li>
+                            <li>
+                                <span>Kỳ xét hạng tiếp theo vào 24/06/2021</span>
+                            </li>
+                        </ul>
+                    </div>
+                </a>
+            </div> -->
+        </div>
+        <!-- <div class="userbalance">
+            Tài khoản tin rao:
+            <span>0</span><br>
+            Tài khoản ngoài tin rao:
+            <span>0</span><br>
+            Tài khoản KM1:
+            <span>0</span><br>
+            Tài khoản KM2:
+            <span>40.000</span><br>
+        </div>
+        <a class="bluebotton" href="#">Nạp tiền</a> -->
+    </div>
+</div>
