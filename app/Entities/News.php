79a37dd93cc9a130a9f5ab244855bc78a98964a7
nguyendung <nvd.dung.1999@gmail.com>
2020-06-29 01:45:54 +0000

    Dungvirgo/template

--- app/Entities/News.php
+++ app/Entities/News.php
@@ -5,11 +5,19 @@ namespace App\Entities;
 use Illuminate\Database\Eloquent\Model;
 use Carbon\Carbon;
 use App\User;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class News extends Model
 {
+
+    const PENDING = 1;
+    const ACTIVE = 2;
+    const CANCEL = 3;
+
+    use Sluggable;
+
     protected $fillable = [
-        'title', 'slug', 'meta_content','content','thumbnail','status'
+        'title', 'slug', 'meta_content','content','thumbnail','status', 'category_id', 'user_id', 'project_id'
     ];
 
     const DECLINE = 1;
@@ -36,5 +44,10 @@ class News extends Model
     public function getStatusNameAttribute()
     {
         return self::STATUSES[$this->status];
+    }
+    
+    public function sluggable()
+    {
+        return ['slug' => ['source' => 'title']];
     }
 }
--- routes/api/project.php
+++ routes/api/project.php
@@ -2,4 +2,7 @@
 
 Route::group(['guard' => 'api', 'middleware' => ['auth:api']], function () {
     Route::post('/project/create', 'Api\ProjectController@createProject')->name('api.project.create');
+
 });
+
+Route::get('/project/search', 'Api\ProjectController@searchByName');
