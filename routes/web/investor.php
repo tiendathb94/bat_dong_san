2d488aef9034d0406b638588c87bf9babc7ba57d
Quyen bui <buiquyen1995@gmail.com>
2020-06-30 18:40:54 +0700

    Finish update investor

--- routes/web/investor.php
+++ routes/web/investor.php
@@ -1,7 +1,11 @@
 <?php
 
 Route::group(['guard' => 'web', 'middleware' => ['auth', 'permission']], function () {
-    Route::get('/investor/create', 'Web\InvestorController@create')->defaults('_config', [
-        'view' => 'default.pages.investor.create'
-    ])->name('pages.investor.create');
+    Route::get('/investor/create', 'Web\InvestorController@create')
+        ->defaults('_config', ['view' => 'default.pages.investor.create'])
+        ->name('pages.investor.create');
+
+    Route::get('/investor/update/{investorId}', 'Web\InvestorController@update')
+        ->defaults('_config', ['view' => 'default.pages.investor.update'])
+        ->name('pages.investor.update');
 });
