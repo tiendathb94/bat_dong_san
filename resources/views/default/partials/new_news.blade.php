ac9fe2b8ba6f22b24fef682586571c88f388f21d
Thang Bui <thangbui.071192@gmail.com>
2020-07-03 09:21:00 +0700

    fix review

--- resources/views/default/partials/new_news.blade.php
+++ resources/views/default/partials/new_news.blade.php
@@ -1,25 +1,27 @@
 @php($firstNews = $news->first())
-<div class="container">
-    <div class="row">
-        <div class="col-6">
-            @include('default.partials.title_category', ['title' => 'Tin nổi bật'])
-            <div class="row">
-                <div class="col-12">
-                    <img src="{{ $firstNews->thumbnail_path }}" alt="" class="w-100 mb-3">
-                    <h4><a class="text-default" title="{{ $firstNews->title }}" href="{{ route('news.show', [$firstNews->category->slug, $firstNews->slug]) }}">{{ $firstNews->title }}</a></h4>
-                    <p class="news-time">{{ getDifferentTime($firstNews->created_at) }}</span>
-                    <p>{{ $firstNews->meta_content }}</p>
-                </div>
-                @foreach($news->except($firstNews->id) as $childNews)
-                    <div class="col-4 mb-3">
-                        <img src="{{ $childNews->thumbnail_path }}" alt="" class="w-100 mb-3">
-                        <a class="text-default" title="{{ $childNews->title }}" href="{{ route('news.show', [$childNews->category->slug, $childNews->slug]) }}">
-                            {{ $childNews->title }}
-                        </a>
-                        <p class="news-time">{{ getDifferentTime($childNews->created_at) }}</span>
+@if($firstNews)
+    <div class="container">
+        <div class="row">
+            <div class="col-6">
+                @include('default.partials.title_category', ['title' => 'Tin nổi bật'])
+                <div class="row">
+                    <div class="col-12">
+                        <img src="{{ $firstNews->thumbnail_path }}" alt="" class="w-100 mb-3">
+                        <h4><a class="text-default" title="{{ $firstNews->title }}" href="{{ route('news.show', [$firstNews->category->slug, $firstNews->slug]) }}">{{ $firstNews->title }}</a></h4>
+                        <p class="news-time">{{ getDifferentTime($firstNews->created_at) }}</span>
+                        <p>{{ $firstNews->meta_content }}</p>
                     </div>
-                @endforeach
+                    @foreach($news->except($firstNews->id) as $childNews)
+                        <div class="col-4 mb-3">
+                            <img src="{{ $childNews->thumbnail_path }}" alt="" class="w-100 mb-3">
+                            <a class="text-default" title="{{ $childNews->title }}" href="{{ route('news.show', [$childNews->category->slug, $childNews->slug]) }}">
+                                {{ $childNews->title }}
+                            </a>
+                            <p class="news-time">{{ getDifferentTime($childNews->created_at) }}</span>
+                        </div>
+                    @endforeach
+                </div>
             </div>
         </div>
     </div>
-</div>
+@endif
